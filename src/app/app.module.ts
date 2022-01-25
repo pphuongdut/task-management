c4d1bee13ee5a70f779a7318c7f98e0eca7ab650
Phuong Tran <phuong.tran@enlabsoftware.com>
2022-01-25 17:24:09 +0700

    remove firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { AUTH_SCHEME, ACCESS_TOKEN_KEY } from "@app/shared/constants";
 import { AppLoadService } from "@app/modules/core/services";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 export function initializeApp(injector: Injector) {
     return (): Promise<any> => {
@@ -32,6 +30,7 @@ export function accessTokenGetter() {
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
+        ThemeModule,
         //
         NgxPermissionsModule.forRoot(),
         JwtModule.forRoot({
@@ -44,16 +43,7 @@ export function accessTokenGetter() {
         //
         SharedModule.forRoot(),
         AppRoutingModule,
-        AngularFireModule.initializeApp({
-            apiKey: "AIzaSyA119QFb1tvabBIzAJGhdcG5DQ27vOofBg",
-            authDomain: "task-management-12a98.firebaseapp.com",
-            projectId: "task-management-12a98",
-            storageBucket: "task-management-12a98.appspot.com",
-            messagingSenderId: "746716276552",
-            appId: "1:746716276552:web:3a3214b2242543d51d757d",
-            measurementId: "G-3ERFTN6025",
-        }),
-        AngularFirestoreModule,
+
     ],
     declarations: [AppComponent],
     providers: [
